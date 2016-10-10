30f9ece19605ba304896d145e5cf1b4c4d8a7ddf
Mees <m.brinkhuis@student.tudelft.nl>
2016-10-10 16:19:53 +0200

    Gave more options to the user by being able to ommit at most one parameter

--- columns.js
+++ columns.js
@@ -1,18 +1,31 @@
 
 /**
- * @param id    id of div to apply to
- * @param width width of columns
- * @param gap   size of gap between images
+ * Call this on your div to change the layout of the elements within.
+ * You can also set at most one parameter to be null,
+ * that one will then be generated automatically.
+ * @param id        id of div to apply to
+ * @param width     width of columns
+ * @param gap       size of gap between images
+ * @param columns   amount of columns
  */
-function columns(id, width, gap){
+function columns(id, width, gap, columns){
+
   var div = document.getElementById(id);
   var elements = div.getElementsByTagName("img");
   div.style.position = "relative";
 
-  //calculate amount of columns
-  var columns = Math.floor((div.offsetWidth + gap) / (width + gap));
-  if(columns == 0){
-    columns = 1;
+  //calculate missing values
+  if(width == null){
+    var width = (div.offsetWidth - gap * (columns - 1)) / (columns);
+  }
+  if(gap == null){
+    var gap = (div.offsetWidth - columns * width) / (columns - 1);
+  }
+  if(columns == null){
+    var columns = Math.floor((div.offsetWidth + gap) / (width + gap));
+    if(columns <= 0){
+      columns = 1;
+    }
   }
 
   //initialize an array to keep track of heights
@@ -31,26 +44,25 @@ function columns(id, width, gap){
     curImg.style.top = heights[column];
     heights[column] += getImgHeight(curImg.src, width) + gap;
   }
-}
 
-//get the height of a particular image after width has been changed
-function getImgHeight(path, width){
-  var img = new Image();
-  img.src = path;
-  var resize = width / img.width;
-  img.style.width = width;
-  return Math.round(img.height * resize);
-}
-
-//get index of lowest value of an array
-function getLowest(heights){
-  var min = Number.MAX_VALUE;
-  var index = 0;
-  for(var i in heights){
-    if(heights[i] < min){
-      min = heights[i];
-      index = i;
-    }
+  //get the height of a particular image after width has been changed
+  function getImgHeight(path, width){
+    var img = new Image();
+    img.src = path;
+    var resize = width / img.width;
+    return Math.round(img.height * resize);
   }
-  return index;
+
+  //get index of lowest value of an array
+  function getLowest(heights){
+    var min = Number.MAX_VALUE;
+    var index = 0;
+    for(var i in heights){
+      if(heights[i] < min){
+        min = heights[i];
+        index = i;
+      }
+    }
+    return index;
+  }
 }
